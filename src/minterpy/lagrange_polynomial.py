cb01e2b173120ac04e2d84416f8c4fa35790506c
MrMinimal64 <github@michelfe.it>
2021-05-02 23:38:55 +0200

    BUGFIX: update Lagrange polynomial
    use array reprentation of trafo op.
    add missing import statement

--- src/minterpy/lagrange_polynomial.py
+++ src/minterpy/lagrange_polynomial.py
@@ -3,18 +3,22 @@ LagrangePolynomial class
 """
 from typing import Optional
 
+import numpy as np
+
 import minterpy
+from minterpy.canonical_polynomial import _match_dims, _matching_internal_domain
 from minterpy.global_settings import ARRAY
+from minterpy.multi_index_utils import insert_lexicographically
 from minterpy.multivariate_polynomial_abstract import MultivariatePolynomialSingleABC
 from minterpy.utils import newt_eval
 from minterpy.verification import verify_domain
-from minterpy.multi_index_utils import insert_lexicographically
-from minterpy.canonical_polynomial import _match_dims, _matching_internal_domain
 
 __all__ = ['LagrangePolynomial']
 
+
 def dummy():
     raise NotImplementedError(f"This feature is not implemented yet.")
+
 
 # TODO: part of polynomial utils?
 # TODO: optimize with numba
@@ -41,6 +45,7 @@ def _union_of_exponents(exp1, exp2):
 
     return res_exp, res_map
 
+
 # TODO : Generalize to handle multiple polynomials (2D coeffs)
 def _generic_lagrange_add(exp1, coeff1, exp2, coeff2):
     if len(coeff2) > len(coeff1):
@@ -57,6 +62,7 @@ def _generic_lagrange_add(exp1, coeff1, exp2, coeff2):
 
     return res_exp, res_coeff
 
+
 # TODO : poly2 can be of a different basis?
 def _lagrange_add(poly1, poly2):
     """Addition of two polynomials in Lagrange basis.
@@ -69,6 +75,7 @@ def _lagrange_add(poly1, poly2):
     else:
         raise NotImplementedError("Addition is not implemented for lagrange polynomials with different domains")
 
+
 def _lagrange_sub(poly1, poly2):
     """Subtraction of two polynomials in Lagrange basis.
     """
@@ -79,6 +86,7 @@ def _lagrange_sub(poly1, poly2):
         return LagrangePolynomial(res_c, res_mi, internal_domain=p1.internal_domain, user_domain=p1.user_domain)
     else:
         raise NotImplementedError("Subtraction is not implemented for lagrange polynomials with different domains")
+
 
 def _lagrange_mul(poly1, poly2):
     """Multiplication of two polynomials in Lagrange basis.
@@ -104,13 +112,15 @@ def _lagrange_mul(poly1, poly2):
         num_points_poly1 = p1.unisolvent_nodes.shape[0]
         scale_up_poly1 = np.zeros((num_points_res, num_points_poly1))
         for i in range(num_points_poly1):
-            scale_up_poly1[:, i] = newt_eval(res_grid.unisolvent_nodes, l2n_1.transformation_operator.to_array()[:, i],
+            scale_up_poly1[:, i] = newt_eval(res_grid.unisolvent_nodes,
+                                             l2n_1.transformation_operator.array_repr_full[:, i],
                                              p1.multi_index.exponents, p1.grid.generating_points)
 
         num_points_poly2 = p2.unisolvent_nodes.shape[0]
         scale_up_poly2 = np.zeros((num_points_res, num_points_poly2))
         for i in range(num_points_poly2):
-            scale_up_poly2[:, i] = newt_eval(res_grid.unisolvent_nodes, l2n_2.transformation_operator.to_array()[:, i],
+            scale_up_poly2[:, i] = newt_eval(res_grid.unisolvent_nodes,
+                                             l2n_2.transformation_operator.array_repr_full[:, i],
                                              p2.multi_index.exponents, p2.grid.generating_points)
 
         res_c = np.multiply(scale_up_poly1 @ p1.coeffs, scale_up_poly2 @ p2.coeffs)
@@ -124,6 +134,7 @@ def _lagrange_mul(poly1, poly2):
 # TODO redundant
 lagrange_generate_internal_domain = verify_domain
 lagrange_generate_user_domain = verify_domain
+
 
 class LagrangePolynomial(MultivariatePolynomialSingleABC):
     """
